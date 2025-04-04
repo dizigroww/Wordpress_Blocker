83bd1829f87e6726d42011eaa247e45c86e4d79f
dizigroww <dizigrowwofficial@gmail.com>
2025-04-04 09:54:44 +0530

    test update blocker.js

--- blocker.js
+++ blocker.js
@@ -1,26 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-    alert("Working");
-    document.body.innerHTML = `
-      <div style="
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        font-family: sans-serif;
-        background-color: #111;
-        color: #f00;
-        text-align: center;
-        padding: 20px;
-      ">
-        <div>
-          <h1>Website Error</h1>
-          <p>We're sorry, but the website is currently unavailable. Please try again later.</p>
-        </div>
-      </div>
-    `;
+// document.addEventListener("DOMContentLoaded", function () {
+//     alert("Working");
+//     document.body.innerHTML = `
+//       <div style="
+//         display: flex;
+//         justify-content: center;
+//         align-items: center;
+//         height: 100vh;
+//         font-family: sans-serif;
+//         background-color: #111;
+//         color: #f00;
+//         text-align: center;
+//         padding: 20px;
+//       ">
+//         <div>
+//           <h1>Website Error</h1>
+//           <p>We're sorry, but the website is currently unavailable. Please try again later.</p>
+//         </div>
+//       </div>
+//     `;
   
-    // // Stop all scripts
-    // const scripts = document.querySelectorAll("script");
-    // scripts.forEach(script => script.remove());
-  });
+//     // // Stop all scripts
+//     // const scripts = document.querySelectorAll("script");
+//     // scripts.forEach(script => script.remove());
+//   });
   
+alert("working");
